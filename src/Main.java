39ffb03037909ba1a8e325295dbc4b00a2a085a2
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-10-04 08:55:46 -0400

    day1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String playerA = "";
+        String playerB = "";
+        boolean validA = false;
+        boolean validB = false;
+        boolean onward = false;
+
+        do {
+            do {
+                System.out.println("Enter your move");
+                scan.nextLine();
+                if (playerA.equalsIgnoreCase("s")) {
+
+                }
+            } while (!validA);
+        } while(!onward);
     }
 }
